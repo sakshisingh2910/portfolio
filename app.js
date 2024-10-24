c7af6c38f93cb78eb9ac84bd916d8d9233926c69
sakshisingh2910 <146133044+sakshisingh2910@users.noreply.github.com>
2024-10-24 18:22:48 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const sidebarIcon = document.querySelector('.sidebar-icon');
+const sidebar = document.querySelector('.sidebar');
+
+sidebarIcon.addEventListener('click', () => {
+    sidebar.classList.toggle('active');
+});
+
+
+.footer {
+    position : fixed;
+    display : flex;
+}
